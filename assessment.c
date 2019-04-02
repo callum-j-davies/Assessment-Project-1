895ef5b7cee363f43cdad481ccb1867dcd3ec866
Callum Davies <callum.davies@uon.edu.au>
2019-04-02 00:45:18 +0000

    Beginning of code base.

--- assessment.c
+++ assessment.c
@@ -1,1 +1,42 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+int firstchoice;
+int secondchoice;
+printf("Do you want Rotation cipher or Substitution cipher? (0/1): %d\n");
+scanf("%d\n", &firstchoice);
+if (firstchoice==0)
+{
+    printf("Do you want to encrypt of decrypt? (0/1): ");  
+    int x = scanf("%d", &secondchoice);
+    if (x==0)
+    {
+        //run encryption
+    }
+    if (x==1)
+    {
+        //run decryption
+    }
+    else printf("Unknown input");
+}
+if (firstchoice==1)
+{
+    printf("Do you want to encrypt of decrypt? (0/1): ");  
+    int x = scanf("%d\n", &secondchoice);
+    if (x==0)
+    {
+        //run encryption
+    }
+    if (x==1)
+    {
+        //run decryption
+    }
+    else printf("Unknown input");
+}
+else printf("Unknown input");
+return 0;
+}
 
