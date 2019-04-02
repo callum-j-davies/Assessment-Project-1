705a5a64f7203b6ce73acacfa3efd51fba0e74b0
Callum Davies <callum.davies@uon.edu.au>
2019-04-02 01:13:22 +0000

    Optimisation

--- assessment.c
+++ assessment.c
@@ -1,42 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-
+char Rotcipher(char input);
+char Subcipher(char input);
 
 int main()
 {
-int firstchoice;
-int secondchoice;
-printf("Do you want Rotation cipher or Substitution cipher? (0/1): %d\n");
-scanf("%d\n", &firstchoice);
-if (firstchoice==0)
-{
-    printf("Do you want to encrypt of decrypt? (0/1): ");  
-    int x = scanf("%d", &secondchoice);
-    if (x==0)
+    int firstchoice;
+    int secondchoice;
+    printf("Do you want Rotation cipher or Substitution cipher? Enter (0/1): "); 
+    scanf("%d\n", &firstchoice);
+    if (firstchoice==0)
     {
-        //run encryption
+        printf("Do you want to encrypt of decrypt? (0/1): ");  
+        int x = scanf("%d", &secondchoice);
+        if (x==0)
+        {
+            //run encryption
+        }
+        if (x==1)
+        {
+            //run decryption
+        }
+        else printf("Unknown input");
     }
-    if (x==1)
+    if (firstchoice==1)
     {
-        //run decryption
+        printf("Do you want to encrypt of decrypt? (0/1): ");  
+        int x = scanf("%d\n", &secondchoice);
+        if (x==0)
+        {
+            //run encryption
+        }
+        if (x==1)
+        {
+            //run decryption
+        }
+        else printf("Unknown input");
     }
     else printf("Unknown input");
-}
-if (firstchoice==1)
-{
-    printf("Do you want to encrypt of decrypt? (0/1): ");  
-    int x = scanf("%d\n", &secondchoice);
-    if (x==0)
-    {
-        //run encryption
-    }
-    if (x==1)
-    {
-        //run decryption
-    }
-    else printf("Unknown input");
-}
-else printf("Unknown input");
-return 0;
+    return 0;
 }
 
