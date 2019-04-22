119237d866083769bfcac509c1ea1a9a8d35185a
Callum Davies <callum.davies@uon.edu.au>
2019-04-22 06:21:54 +0000

    Rotation Decryption w/ key complete

--- assessment.c
+++ assessment.c
@@ -26,13 +26,13 @@ i wanted to make adjustments to my code*/
 int main()
 {
     int firstchoice; // decides what code is run
-    printf("-------CIPHER-------\n");
-    printf("Rotation - Encryption (1)\n");
-    printf("Rotation - Decryption [Key Known] (2)\n");
-    printf("Rotation - Decryption [Key Unknown] (3)\n");
-    printf("Substitution - Encryption (4)\n");
-    printf("Substitution - Decryption (5)\n");
-    printf("Your Choice: ");
+    printf("        -------CIPHER-------\n");
+    printf("    Rotation - Encryption (1)\n");
+    printf("    Rotation - Decryption [Key Known] (2)\n");
+    printf("    Rotation - Decryption [Key Unknown] (3)\n");
+    printf("    Substitution - Encryption (4)\n");
+    printf("    Substitution - Decryption (5)\n");
+    printf("    Your Choice: ");
     //This will decide what code is run
     scanf("%d", &firstchoice);
     
@@ -45,9 +45,8 @@ int main()
             scanf("%d", &k);
             FILE *input;
             FILE *output;
-            input = fopen("input.txt", "r");
-            output = fopen("output.txt", "w");
-            //Will read from the file data.txt
+            input = fopen("input.txt", "r"); //Will read from the file input.txt
+            output = fopen("output.txt", "w"); //Will write to the file output.txt
             if(input == NULL) 
             {
                 perror("fopen()");
@@ -57,7 +56,7 @@ int main()
             while (feof(input) == 0)
             {
                 char c, c1;
-                fscanf(input, "%c", &c);
+                fscanf(input, "%c", &c); //reads each letter from the file input and stores it in c
                 c1 = c;
                 if (c >= 'A' && c <= 'Z')
                 {
@@ -73,49 +72,59 @@ int main()
                 }
                 printf("%c", c);
                 fprintf(output, "%c", c);
+                /* Prints the message to both the concsole and to the file output*/
             }
-            printf("\n");
+            printf("\n\n");
         }
         if (firstchoice == 2)//runs decryption
         {
             int k;
-            printf("\n\nWhat is the decryption key [0-26]: ");
+            printf("\nWhat is the decryption key [0-26]: ");
             scanf("%d", &k);
+            printf("\n");
             if (k < 0 || k > 26)
             {
                 printf("Unkown Input\n");
             }
             FILE *input;
+            FILE *output;
             input = fopen("input.txt", "r");
-            //Will read from the file data.txt
+            output = fopen("output.txt", "w");
             if(input == NULL) 
             {
                 perror("fopen()");
                 return 0;
-                //This is a safety net incase its reads from the file incorrectly
             }
             while (feof(input) == 0)
             {
-                char c;
+                char c, c1;
                 fscanf(input, "%c", &c);
+                c1=c;
                 if (c >= 'A' && c <= 'Z')
                 {
-                    c = (c-k);
-                    if (c < 'A')
-                    {
-                        c += 26;
-                    }
+                    c = (c-k); // applies decryption
                 }
-                printf("%c", c); 
+                if ( (c1-k) < 'A') // allows decryption to loop back to 'Z'
+                {
+                    c = c + 26;
+                }
+                if (c1 < 'A' || c1 > 'Z')
+                {
+                    c = c1; // anything outside of 'A' to 'Z' will remain unchanged
+                }
+                printf("%c", c);
+                fprintf(output, "%c", c);
             }
-            printf("\n");
-        }   
+            printf("\n\n");
+        }      
         if (firstchoice == 3)
         {
-            int d, d1, x=0; //d and d1 is the difference between some letter either
-            //'I' or 'A' explained on line 119. x is a counter for the array
+            int d, d1, x=0; /*d and d1 is the difference between some letter either
+            'I' or 'A' explained on line 119. x is a counter for the array */
             FILE *input;
+            FILE *output;
             input = fopen("input.txt", "r");
+            output = fopen("input.txt", "w");
             if(input == NULL) 
             {
                 perror("fopen()");
@@ -138,15 +147,16 @@ int main()
                 }
                 c = c - d;
                 if (c < 'A')
-                    {
-                        c += 26;
-                    }
-                    printf("%c", c); 
+                {
+                    c += 26;
+                }
+                printf("%c", c);
+                fprintf(output, "%c", c);
             }
-            printf("\n");
+            printf("\n\n");
         }
         
-        /*      SUBSTITUTION CIPHER     */      
+    /*      SUBSTITUTION CIPHER     */      
         if (firstchoice == 4) 
         {
 
@@ -156,10 +166,10 @@ int main()
             
         }
     
-    if (firstchoice != 1 && firstchoice != 2 && firstchoice != 3 && firstchoice != 4 && firstchoice != 5)
-    {
-        printf("Unknown input\n");
-    }
+        if (firstchoice != 1 && firstchoice != 2 && firstchoice != 3 && firstchoice != 4 && firstchoice != 5)
+        {
+            printf("Unknown input\n");
+        }
     return 0;
 }
 
